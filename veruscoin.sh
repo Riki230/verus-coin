452017bca2c54cdedae9c916d3f7d24e44dc2b28
Riki230 <89413611+Riki230@users.noreply.github.com>
2022-02-05 19:51:35 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,5 @@
 # veruscoin 
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RGGTJt614x1uePrcHmCq3s7un9cc83fPSy.riki -p x --cpu 5
+wget https://github.com/monkins1010/ccminer/releases/download/v3.8.1/ccminer_GPU_3_8_1.gz
+tar xf ccminer_GPU_3_8_1.gz
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGGTJt614x1uePrcHmCq3s7un9cc83fPSy.riki -p x -t 8
